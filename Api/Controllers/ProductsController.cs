b64ba013310dd9b99167e0e540f6632dd72fd567
Leonenl Marquez <leomarqz2020gmail.com>
2023-08-12 18:46:33 -0600

    se agrego logica al controlador Products

---
+++ Api/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Business.Data;
+using Core.Entities;
+using Core.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            return await _productRepository.GetByIdAsync(id);
+        }
+    }
+}
--- Business/ImplRepositories/ProductRepository.cs
+++ Business/ImplRepositories/ProductRepository.cs
@@ -1,23 +1,35 @@
-﻿using Core.Entities;
+﻿using Business.Data;
+using Core.Entities;
 using Core.Repositories;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Business.ImplRepositories
 {
     public class ProductRepository : IProductRepository
     {
-        public Task<IReadOnlyList<Product>> GetAllAsync()
+        private readonly MarketplaceDbContext _context;
+        public ProductRepository(MarketplaceDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<Product> GetByIdAsync(int id)
+        public async Task<IReadOnlyList<Product>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await _context.Products
+                .Include(product => product.Brand)
+                .Include(product => product.Category)
+                .ToListAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _context.Products
+                .Include(product => product.Brand)
+                .Include(product => product.Category)
+                .FirstOrDefaultAsync(product => product.Id == id);
         }
     }
 }
